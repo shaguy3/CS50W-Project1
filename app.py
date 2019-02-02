0fa3ac0e2809e4f05a17219791e3809b8d69f0cc
guy <shaguy3@gmail.com>
2019-02-02 20:28:16 +0200

    Worked on the front end of the website. added bootstrap classes and image to the site.

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def home_page():
-	return render_template("index.html")
+	return render_template('home.html', title='home')
+
+@app.route("/about")
+def about_page():
+	return render_template('about.html', title='about')
 
 if __name__ == '__main__':
 	app.run(debug=True)
