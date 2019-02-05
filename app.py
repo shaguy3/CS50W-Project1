76a8901b584bd1dbe4f1cb81916fad30a5e78e1b
guy <shaguy3@gmail.com>
2019-02-06 00:06:16 +0200

    Added database functionality. Added an import_books method that imports books from a csv file and adds them to the database.

--- app.py
+++ app.py
@@ -1,47 +1,13 @@
 from flask import Flask, render_template, url_for, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
 from forms import RegisterationForm, LoginForm
+from models import User, Book, Review
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '67fbe61600452e3f1a211569e15aec78'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 db = SQLAlchemy(app)
-
-
-class User(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	username = db.Column(db.String(20), unique=True, nullable=False)
-	email = db.Column(db.String(120), unique=True, nullable=False)
-	image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
-	password = db.Column(db.String(60), nullable=False)
-	reviews = db.relationship('Review', backref='reviewer', lazy=True)
-
-	def __repr__(self):
-		return f"User('{self.username}', '{self.email}', '{self.image_file}')"
-
-
-class Book(db.Model):
-	isbn = db.Column(db.Integer, primary_key=True)
-	title = db.Column(db.String(60), nullable=False)
-	author = db.Column(db.String(60), nullable=False, default='Unknown')
-	publish_year = db.Column(db.Integer, nullable=False, default='Unknown')
-	book_reviews = db.relationship('Review', backref='reviewed book', lazy=True)
-
-	def __repr__(self):
-		return f"Book('{self.title}', '{self.author}', '{self.publish_year}')"
-
-
-class Review(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	book_isbn = db.Column(db.Integer, db.ForeignKey('book.isbn'), nullable=False)
-	reviewing_user = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-	review_score = db.Column(db.Integer, nullable=False)
-	review_text = db.Column(db.Text)
-
-	def __repr__(self):
-		return f"Review('{self.book_isbn}', '{self.reviewing_user}', '{self.review_score}')"
-
 
 @app.route('/')
 def home():
